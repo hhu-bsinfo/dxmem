514435e67583f21ceadd6954d8df3e1a31b5af50
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2018-08-06 07:51:38 +0200

    benchmark: put op, fix fill dummy data

--- src/main/java/de/hhu/bsinfo/dxmem/benchmark/operation/Put.java
+++ src/main/java/de/hhu/bsinfo/dxmem/benchmark/operation/Put.java
@@ -1,17 +1,17 @@
 package de.hhu.bsinfo.dxmem.benchmark.operation;
 
 import de.hhu.bsinfo.dxmem.DXMem;
-import de.hhu.bsinfo.dxmem.data.ChunkByteArray;
+import de.hhu.bsinfo.dxmem.data.ChunkBenchmark;
 import de.hhu.bsinfo.dxmem.data.ChunkID;
 import de.hhu.bsinfo.dxmem.data.ChunkState;
 
 public class Put extends AbstractOperation {
-    private final ChunkByteArray m_chunk;
+    private final ChunkBenchmark m_chunk;
 
     public Put(final float p_probability, final int p_batchCount, final boolean p_verifyData, final int p_chunkSize) {
         super("put", p_probability, p_batchCount, p_verifyData);
 
-        m_chunk = new ChunkByteArray(ChunkID.INVALID_ID, p_chunkSize);
+        m_chunk = new ChunkBenchmark(ChunkID.INVALID_ID, p_chunkSize);
     }
 
     @Override
@@ -26,10 +26,7 @@ public class Put extends AbstractOperation {
         m_chunk.setID(cid);
 
         if (p_verifyData) {
-            // write data to chunk for verification
-            for (int i = 0; i < m_chunk.getData().length; i++) {
-                m_chunk.getData()[i] = (byte) i;
-            }
+            m_chunk.fillContents();
         }
 
         executeTimeStart();
