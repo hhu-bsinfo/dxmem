f2d7c0f43b216541bbc4c01b6748abf296f6e2f6
Stefan Nothaas <stefan.nothaas@hhu.de>
2018-08-10 14:12:13 +0200

    Add pinning test

---
+++ src/test/java/de/hhu/bsinfo/dxmem/operations/PinningTest.java
@@ -0,0 +1,50 @@
+package de.hhu.bsinfo.dxmem.operations;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.hhu.bsinfo.dxmem.DXMem;
+import de.hhu.bsinfo.dxmem.DXMemoryTestConstants;
+import de.hhu.bsinfo.dxmem.core.Address;
+
+public class PinningTest {
+    @Test
+    public void pin() {
+        Configurator.setRootLevel(Level.TRACE);
+        DXMem memory = new DXMem(DXMemoryTestConstants.NODE_ID, DXMemoryTestConstants.HEAP_SIZE_SMALL);
+
+        long cid = memory.create().create(16);
+
+        Pinning.PinnedMemory pinnedMemory = memory.pinning().pin(cid);
+
+        Assert.assertTrue(pinnedMemory.isStateOk());
+        Assert.assertNotEquals(Address.INVALID, pinnedMemory.getAddress());
+
+        Assert.assertTrue(memory.analyze().analyze());
+
+        memory.pinning().unpin(pinnedMemory.getAddress());
+
+        Assert.assertTrue(memory.analyze().analyze());
+    }
+
+    @Test
+    public void pin2() {
+        Configurator.setRootLevel(Level.TRACE);
+        DXMem memory = new DXMem(DXMemoryTestConstants.NODE_ID, DXMemoryTestConstants.HEAP_SIZE_SMALL);
+
+        long cid = memory.create().create(16);
+
+        Pinning.PinnedMemory pinnedMemory = memory.pinning().pin(cid);
+
+        Assert.assertTrue(pinnedMemory.isStateOk());
+        Assert.assertNotEquals(Address.INVALID, pinnedMemory.getAddress());
+
+        Assert.assertTrue(memory.analyze().analyze());
+
+        memory.pinning().unpinCID(cid);
+
+        Assert.assertTrue(memory.analyze().analyze());
+    }
+}
