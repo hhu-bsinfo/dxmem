fbd752871f00383dfc6fe5b2ca2d8ca618d7c34f
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2018-10-23 22:00:40 +0200

    CIDTable: Align level 0 LID tables for extreme performance boost
    The CAS operations for read/write locking of chunks were executed
    on unaligned longs and resulted in overall extremely high latencies
    (average as well as 99.9th percentiles). LID level 0 tables are
    now aligned to 8 byte borders which boosts performance by up
    to a factor of 14

--- src/main/java/de/hhu/bsinfo/dxmem/core/CIDTable.java
+++ src/main/java/de/hhu/bsinfo/dxmem/core/CIDTable.java
@@ -41,6 +41,7 @@ import de.hhu.bsinfo.dxutils.serialization.Importer;
 public final class CIDTable implements Importable, Exportable {
     private static final Logger LOGGER = LogManager.getFormatterLogger(CIDTable.class.getSimpleName());
 
+    static final int TABLE_0_ALIGNMENT_BYTES = 8;
     static final byte ENTRY_SIZE = 8;
     static final byte LID_TABLE_LEVELS = 4;
 
@@ -702,20 +703,49 @@ public final class CIDTable implements Importable, Exportable {
         tmp.setLengthField(0);
 
         // TODO reduce pointer length for all levels except 0 to 6 byte because TableEntry is address only
-        if (!m_heap.malloc(LID_TABLE_SIZE, tmp, true)) {
+
+        int alignment = 0;
+
+        // align all level 0 tables containing locks to 8 byte borders
+        // otherwise, the latency for CAS operations sky rockets
+        if (p_level == 0) {
+            alignment = TABLE_0_ALIGNMENT_BYTES;
+        }
+
+        if (!m_heap.malloc(LID_TABLE_SIZE + alignment, tmp, true)) {
             return false;
         }
+
+        if (alignment > 0) {
+            alignment = -1;
+
+            for (int i = 0; i < TABLE_0_ALIGNMENT_BYTES; i++) {
+                if ((tmp.getAddress() + i) % TABLE_0_ALIGNMENT_BYTES == 0) {
+                    alignment = i;
+                    break;
+                }
+            }
+
+            if (alignment == -1) {
+                throw new IllegalStateException("Alignment failed for address: " + tmp.getAddress());
+            }
+        }
+
+        // null table
+        m_heap.set(tmp.getAddress(), 0, LID_TABLE_SIZE + alignment, (byte) 0);
+
+        // expose aligned origin only
+        tmp.setAddress(tmp.getAddress() + alignment);
 
         p_entry.clear();
         p_entry.setAddress(tmp.getAddress());
 
-        // null table
-        m_heap.set(p_entry.getAddress(), 0, LID_TABLE_SIZE, (byte) 0);
-        m_status.m_totalPayloadMemoryTables += LID_TABLE_SIZE;
+        m_status.m_totalPayloadMemoryTables += LID_TABLE_SIZE + alignment;
         m_status.m_tableCountLevel[p_level]++;
         m_status.m_totalTableCount++;
 
-        LOGGER.trace("Created LID table size %d at %X", LID_TABLE_SIZE, p_entry.getAddress());
+        LOGGER.trace("Created LID table size %d at %X (alignment offset +" + alignment, LID_TABLE_SIZE + alignment,
+                p_entry.getAddress());
 
         return true;
     }
